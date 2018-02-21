b264fb9c7a228bcf6b0c69e8749aaae3c3f98cfb
Matthew <matthewjkane@live.com>
2018-02-21 13:30:55 -0800

    added CheckForWinner function and isBoardFull function

--- main.js
+++ main.js
@@ -1,12 +1,34 @@
 
-function verticalWinCondition(playerCoin, column){
+function checkForWinner(player, row, column){
+    if(horizontalCheck(player, row) === "true")
+        console.log("You sir are a winner");
+    else if(verticalWinCondition(player, column) === "true")
+        console.log("You are a vertical winner");
+    else if(diagonalCheckDownLeft(player, row, column)==="true")
+        console.log("You are a forward slash winner");
+    else(diagonalCheckDownRight(player, row, column) === "true")
+        console.log("You sir are a back slash winner");
+}
+
+function isBoardFull(){
+    for(var i = 0; i < masterArray.length; i++){
+        for(var j = 0; j < masterArray[0].length; j++){
+            if(masterArray[i][j] === null)
+                return false;
+        }
+    }
+    return true;
+}
+
+
+function verticalWinCondition(player, column){
     var coinCount=null;
     var winCondition=false;
     for (var i =0; i<masterArray[column].length; i++){
         if(coinCount===4) {
             winCondition = true;
             return winCondition;
-        } else if(masterArray[column][i]===playerCoin){
+        } else if(masterArray[column][i]===player){
             coinCount++
         } else{
             coinCount=0;
@@ -38,14 +60,14 @@ function diagonalCheckDownLeft(player, row, column){
     return winCondition;
 }
 
-function diagonalCheckDownRight(playerCoin, column, row){
+function diagonalCheckDownRight(player, row, column){
     var coinCount=null;
     var winCondition=false;
     row=row+5;
     column = column-5;
     while(row>=0){
         if(row<=5 && column>=0 && column<=6){
-            if(masterArray[column][row]===playerCoin){
+            if(masterArray[column][row]===player){
                 coinCount++;
                 if(count===4){
                     wincondition=true;
