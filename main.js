58f109b9b250f67404fe340ff091d575a853ad1f
Matthew <matthewjkane@live.com>
2018-02-21 12:30:07 -0800

    added forward diagonal win condition check

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
 
-<<<<<<< HEAD
-
 function verticalWinCondition(playerCoin, column){
     var coinCount=null;
     var winCondition=false;
@@ -16,17 +14,39 @@ function verticalWinCondition(playerCoin, column){
     }
 }
 
-function diagonalCheck(playerCoin, column, row){
-    var coinCount=null;
-    var wincondition=false;
-
+function diagonalCheck(playerCoin, column, row) {
+    var coinCount = null;
+    var wincondition = false;
+}
+function diagonalCheckForward(player, row, column){
+    row += 5;
+    column += 5;
+    var count = 0;
+    var win = false;
+    while(row >= 0){
+        if(row <= 5 && column >= 0 && column <= 6){
+            if(player === masterArray[column][row]){
+                count++;
+                if(count === 4){
+                    win = true;
+                    return win;
+                }
+            } else {
+                count = 0;
+            }
+        }
+        column--;
+        row--;
+    }
+    return false;
+    }
 
 // Function to check horizontal win condition. Player to test and row the coin as added to are parameters.
 function horizontalCheck(player, row){
     var count = 0;
     var winCondition = false;
-    for (var i = 0; i < masterArray.length; i++){
-        if(player === masterArray[i][row]){
+    for (var col = 0; i < masterArray.length; col++){
+        if(player === masterArray[col][row]){
             count++;
             if(count === 4){
                 wincondition = true;
