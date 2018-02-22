817033033ffb270e9344c9d96fbb50ab53d626b0
Hanran <hanrankim@gmail.com>
2018-02-21 16:29:29 -0800

    added modal

--- main.js
+++ main.js
@@ -116,12 +116,50 @@ $(document).ready(initializeApp);
 
 function initializeApp() {
     attachEventHandlers();
+
 }
 
 function attachEventHandlers(){
     $('.col').on('click', columnClicked);
+    $('#myBtn').on('click', function(){
+        $('#myModal').addClass('hidden');
+    })
 }
 
 function columnClicked(){
     console.log('column clicked');
 }
+
+// Modal box
+
+
+
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+    modal.style.display = "block";
+    console.log('button clicked')
+}
+
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+
